b88abc92c0890ade7e86001fe8dc2e79c0d0d182
silvaahzo <rafael.gru2021@gmail.com>
2025-08-04 11:36:17 -0300

    Add files via upload

---
+++ while.py
@@ -0,0 +1,48 @@
+x = 1
+while x <= 2500:
+    print(x)
+    print('silvaah_zo...')
+    x = x + 1
+
+
+
+import time
+print("iniciando contagem regressiva do foguete")
+x = 10
+while x >= 1:
+    print(x)
+    x = x - 1
+
+
+x  = 1
+while x <= 100:
+    print(x)
+    x = x + 1
+
+
+
+def numeros_impares():
+     fim = 9 
+     x = 0 
+     while x <= fim:
+          if x % 2 == 0:
+               print()
+     else:
+            print()
+            x = x + 1 
+     numeros_impares()
+
+
+
+
+
+def arvore_de_natal():
+     linha = 1
+     while linha <= 10:
+          coluna = 1
+          while coluna <= linha:
+               print('🍏', end="")
+               coluna = coluna + 1  
+          print()
+     linha = linha + 1
+arvore_de_natal()
