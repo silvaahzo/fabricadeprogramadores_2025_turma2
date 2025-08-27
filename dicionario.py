96f7d6f0f423a3f003d5887da2a51162051bc909
silvaahzo <rafael.gru2021@gmail.com>
2025-08-27 10:19:09 -0300

    dicionario rafael

---
+++ dicionario.py
@@ -0,0 +1,13 @@
+tabela = {"alface": 0.45,
+            "batata": 1.20,
+            "tomate": 2.30,
+            "feijão": 1.50}
+print(tabela["alface"])
+print(tabela["batata"])
+print(tabela["tomate"])
+print(tabela["feijão"])
+tabela['limão'] = 0.30
+print(tabela)
+print("Manga" in tabela)
+print("alface" in tabela)
+
